eb6f8b63d28405a526da3e3621150de1e1626e20
tsynik <tsynik@gmail.com>
2015-11-04 08:37:43 +0300

    add minidlna distclean target
    for better cleaning

--- src/router/rules/minidlna.mk
+++ src/router/rules/minidlna.mk
@@ -9,6 +9,14 @@ minidlna: zlib
 minidlna-clean:
 	cd minidlna && make clean
 
+minidlna-distclean:
+	cd minidlna && make distclean
+	-rm -f $(TOP)/minidlna/libogg-1.3.0/Makefile
+	-rm -f $(TOP)/minidlna/sqlite-3.6.22/Makefile	
+	-rm -f $(TOP)/minidlna/libvorbis-1.3.3/Makefile	
+	-rm -f $(TOP)/minidlna/flac-1.2.1/Makefile	
+	-rm -f $(TOP)/minidlna/jpeg-8/Makefile	
+
 minidlna-install:
 	cd minidlna && make install TARGETDIR=$(TOP)/$(ARCH)-uclibc/install/minidlna
 	install -D minidlna/config/minidlna.webnas $(INSTALLDIR)/minidlna/etc/config/minidlna.webnas
