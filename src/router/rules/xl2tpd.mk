8c8e3874ca446a7a4dc37974d9ffb8d9d1d7b9db
tsynik <tsynik@gmail.com>
2015-12-17 12:59:05 +0300

    xl2tpd with some debug info in syslog
    Also adds xl2tp-control to firmware

--- src/router/rules/xl2tpd.mk
+++ src/router/rules/xl2tpd.mk
@@ -3,7 +3,8 @@ xl2tpd-configure:
 	@true
 
 xl2tpd:
-	$(MAKE) -C xl2tpd CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -fno-builtin -Wall -DSANITY -DLINUX -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(LINUXDIR)/include/ -DUSE_KERNEL -DIP_ALLOCATION -I$(TOP)/libpcap" LDFLAGS+="-L$(TOP)/libpcap"
+#	$(MAKE) -C xl2tpd CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -fno-builtin -Wall -DSANITY -DLINUX -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(LINUXDIR)/include/ -DUSE_KERNEL -DIP_ALLOCATION -I$(TOP)/libpcap" LDFLAGS+="-L$(TOP)/libpcap"
+	$(MAKE) -C xl2tpd CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -fno-builtin -Wall -DNEED_PRINTF -DDEBUG_CLOSE -DSANITY -DLINUX -DDEBUG_PPPD -DTRUST_PPPD_TO_DIE -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(LINUXDIR)/include/ -DUSE_KERNEL -DIP_ALLOCATION -I$(TOP)/libpcap" LDFLAGS+="-L$(TOP)/libpcap" LDLIBS+=" -lm"
 
 xl2tpd-clean:
 	$(MAKE) -C xl2tpd clean
@@ -11,5 +12,6 @@ xl2tpd-clean:
 xl2tpd-install:
 	mkdir -p $(INSTALLDIR)/xl2tpd/usr/sbin
 	install -D xl2tpd/xl2tpd $(INSTALLDIR)/xl2tpd/usr/sbin/xl2tpd
+	install -D xl2tpd/xl2tpd-control $(INSTALLDIR)/xl2tpd/usr/sbin/xl2tpd-control
 	mkdir -p ${INSTALLDIR}/xl2tpd/etc/xl2tpd
 	ln -sf /tmp/xl2tpd/xl2tpd.conf ${INSTALLDIR}/xl2tpd/etc/xl2tpd/xl2tpd.conf
